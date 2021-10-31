d0dbb1af0d50afa1b0a9252c646c0133b5f95a6d
Emmanuel Perotto <emmanuelperotto@gmail.com>
2021-10-30 22:48:57 -0300

    Add MySQL db connection on application bootstrap

--- accounts/cmd/api/main.go
+++ accounts/cmd/api/main.go
@@ -4,11 +4,26 @@ import (
 	"accounts/internal/adapter/database/local"
 	"accounts/internal/adapter/web"
 	"accounts/internal/domain/usecase"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"time"
 )
 
 func main() {
+	db, err := setupDB()
+	if err != nil {
+		log.Fatalln("Fatal Error", err.Error())
+	}
+
+	defer func(db *sql.DB) {
+		if err := db.Close(); err != nil {
+			log.Println("Can't close DB Connection")
+		}
+	}(db)
+
 	app := fiber.New()
 	accountWriteOnlyRepository := local.NewAccountWriteOnlyRepository()
 	accountReadOnlyRepository := local.NewAccountReadOnlyRepository()
@@ -21,8 +36,38 @@ func main() {
 	app.Post("/accounts", accountHandler.CreateAccount)
 	app.Get("/accounts/:id", accountHandler.GetAccount)
 
-	err := app.Listen(":3000")
+	err = app.Listen(":3000")
 	if err != nil {
 		log.Fatalln("Error starting server: ", err.Error())
 	}
+}
+
+func setupDB() (db *sql.DB, err error) {
+	const (
+		driver   = "mysql"
+		user     = "example"
+		password = "secret123"
+		host     = "localhost"
+		port     = "3306"
+		dbName   = "accounts"
+	)
+
+	db, err = sql.Open(driver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName))
+
+	if err != nil {
+		log.Println("Error opening DB", err.Error())
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("Error pinging DB", err.Error())
+		return
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return
 }
--- accounts/go.mod
+++ accounts/go.mod
@@ -6,6 +6,7 @@ require github.com/gofiber/fiber/v2 v2.20.2
 
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
