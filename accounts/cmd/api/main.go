db245c77e661abd00bdb7492cbcc34fd50971593
Emmanuel Perotto <emmanuelperotto@gmail.com>
2021-11-08 23:18:38 -0300

    Add localstack

--- accounts/cmd/api/main.go
+++ accounts/cmd/api/main.go
@@ -5,15 +5,67 @@ import (
 	"accounts/internal/adapter/web"
 	"accounts/internal/domain/usecase"
 	"accounts/internal/infra"
+	"context"
 	"database/sql"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
 	"github.com/gofiber/fiber/v2"
 	"log"
 )
 
+// SNSListTopicsAPI defines the interface for the ListTopics function.
+// We use this interface to test the function using a mocked service.
+type SNSListTopicsAPI interface {
+	ListTopics(ctx context.Context,
+		params *sns.ListTopicsInput,
+		optFns ...func(*sns.Options)) (*sns.ListTopicsOutput, error)
+}
+
+// GetTopics retrieves information about the Amazon Simple Notification Service (Amazon SNS) topics
+// Inputs:
+//     c is the context of the method call, which includes the Region
+//     api is the interface that defines the method call
+//     input defines the input arguments to the service call.
+// Output:
+//     If success, a ListTopicsOutput object containing the result of the service call and nil
+//     Otherwise, nil and an error from the call to ListTopics
+func GetTopics(c context.Context, api SNSListTopicsAPI, input *sns.ListTopicsInput) (*sns.ListTopicsOutput, error) {
+	return api.ListTopics(c, input)
+}
+
 func main() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithSharedConfigProfile("pocs"),
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(
+			func(service, region string) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: "http://localhost:4566", SigningRegion: region}, nil
+			})),
+	)
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+
+	client := sns.NewFromConfig(cfg)
+
+	input := &sns.ListTopicsInput{}
+
+	results, err := GetTopics(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error retrieving information about the SNS topics:")
+		fmt.Println(err)
+		return
+	}
+
+	for _, t := range results.Topics {
+		fmt.Println(*t.TopicArn)
+	}
+
 	db, err := infra.ConnectMySQLDB()
 	if err != nil {
-		log.Fatalln("Fatal Error", err.Error())
+		log.Fatalln(err)
 	}
 
 	defer func(db *sql.DB) {
--- accounts/go.mod
+++ accounts/go.mod
@@ -2,11 +2,25 @@ module accounts
 
 go 1.17
 
-require github.com/gofiber/fiber/v2 v2.20.2
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gofiber/fiber/v2 v2.20.2
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.10.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.0.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.5.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sns v1.10.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.9.0 // indirect
+	github.com/aws/smithy-go v1.9.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
